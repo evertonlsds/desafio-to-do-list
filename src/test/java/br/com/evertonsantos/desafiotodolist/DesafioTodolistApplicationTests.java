6c421b25b264e5d3b5d5cbbc278bbdf0e80aae8b
evertonlsds <everton0102@gmail.com>
2024-04-10 12:11:08 -0300

    project structure

---
+++ src/test/java/br/com/evertonsantos/desafiotodolist/DesafioTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.evertonsantos.desafiotodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
